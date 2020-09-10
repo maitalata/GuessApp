fb1f88c4dfc0a65743bb4a60bbf531cbacb1b144
Umar Sunusi  Maitalata <maitalata@gmail.com>
2020-09-10 18:06:38 +0100

    Initial Commit

---
+++ DuessApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DuessApp
+//
+//  Created by Umar Sunusi Maitalata on 10/09/2020.
+//  Copyright © 2020 IWORLDOFTECH. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
